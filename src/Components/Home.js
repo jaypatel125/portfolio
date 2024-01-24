6bbaa39e5b98a4e570334b5161d7e9003f09dddc
Jay Patel <000881881@mohawkcollege.ca>
2024-01-23 22:00:48 -0500

    fix navbar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,33 +1,25 @@
 // Home.js
 import React, { useState, useEffect } from 'react';
-import profilephoto from '../images/IMG_7423.JPG'
+import profilephoto from '../images/IMG_7423.JPG';
+import { TypeAnimation } from 'react-type-animation';
 
 const Home = () => {
-  const [currentText, setCurrentText] = useState('');
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const text = 'ello, world!'; 
-    const delay = 200; 
-
-    const timeout = setTimeout(() => {
-      if (currentIndex < text.length) {
-        setCurrentText((prevText) => prevText + text[currentIndex]);
-        setCurrentIndex((prevIndex) => prevIndex + 1);
-      } else {
-        // Reset when the typing is completed
-        setCurrentText('');
-        setCurrentIndex(0);
-      }
-    }, delay);
-
-    return () => clearTimeout(timeout);
-  }, [currentIndex]);
-
+  
   return (
     <div className='intro-container'>
       <div className='page1'>
-        <h2>H{currentText}</h2>
+        <TypeAnimation
+          sequence={[
+            'Hello!',
+            2000,
+            'नमस्ते!',
+            2000,
+          ]}
+          speed={50}
+          style={{ fontSize: '2em' }}
+          repeat={Infinity}
+        />
+
         <p>
           I'm Jay Patel<br/> A passionate software development student.
         </p>
@@ -38,7 +30,7 @@ const Home = () => {
         <br/>
         <p className='paragraph'>Welcome to my digital space, where I share insights, projects, and experiences from my software development endeavors. Join me on this exciting exploration of technology, creativity, and the endless possibilities that coding offers. Let's embark on a journey of innovation and collaboration in the ever-evolving world of software development.</p>
         <br/>
-        <p className='paragraph'>amazing!</p>
+        <p className='paragraph'>Let's build something amazing!</p>
       </div>
     </div>
   );
