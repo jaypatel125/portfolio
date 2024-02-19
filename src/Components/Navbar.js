358e21bd28f808c2bfa1b63fcc607f83c9919df3
Jay Patel <000881881@mohawkcollege.ca>
2024-02-19 00:27:36 -0500

    fixed menu for phone

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUser, faCode, faFileAlt, faContactCard } from '@fortawesome/free-solid-svg-icons';
-import '../index.css';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faUser,
+  faCode,
+  faFileAlt,
+  faContactCard,
+} from "@fortawesome/free-solid-svg-icons";
+import "../index.css";
 
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState(false);
@@ -19,41 +25,54 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="container">
-        <div className="logo">
-        
-        </div>
+        <div className="logo"></div>
         <div className="menu-icon" onClick={handleShowNavbar}>
           <GiHamburgerMenu className="hamburger" />
         </div>
-        <div className={`nav-elements ${showNavbar && 'active'}`}>
-          <ul className='navlinks'>
+        <div className={`nav-elements ${showNavbar && "active"}`}>
+          <ul className="navlinks">
             <li>
               <NavLink to="/" onClick={closeNavbar}>
-                <FontAwesomeIcon icon={faHome}  style={{ marginRight: '10px' }}/>
+                <FontAwesomeIcon
+                  icon={faHome}
+                  style={{ marginRight: "10px" }}
+                />
                 Home
               </NavLink>
             </li>
             <li>
               <NavLink to="/about" onClick={closeNavbar}>
-                <FontAwesomeIcon icon={faUser}  style={{ marginRight: '10px' }}/>
+                <FontAwesomeIcon
+                  icon={faUser}
+                  style={{ marginRight: "10px" }}
+                />
                 About
               </NavLink>
             </li>
             <li>
               <NavLink to="/projects" onClick={closeNavbar}>
-                <FontAwesomeIcon icon={faCode}  style={{ marginRight: '10px' }}/>
+                <FontAwesomeIcon
+                  icon={faCode}
+                  style={{ marginRight: "10px" }}
+                />
                 Projects
               </NavLink>
             </li>
             <li>
               <NavLink to="/resume" onClick={closeNavbar}>
-                <FontAwesomeIcon icon={faFileAlt}  style={{ marginRight: '10px' }}/>
+                <FontAwesomeIcon
+                  icon={faFileAlt}
+                  style={{ marginRight: "10px" }}
+                />
                 Resume
               </NavLink>
             </li>
             <li>
               <NavLink to="/contact" onClick={closeNavbar}>
-                <FontAwesomeIcon icon={faContactCard}  style={{ marginRight: '10px' }}/>
+                <FontAwesomeIcon
+                  icon={faContactCard}
+                  style={{ marginRight: "10px" }}
+                />
                 Contact
               </NavLink>
             </li>
